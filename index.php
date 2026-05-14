c0a22f6721920510840dac5e864cbdb3b036ecc7
valentina camargo <avcamargo@uniboyaca.co>
2026-05-13 19:09:14 -0500

    Modificando CSS

--- index.php
+++ index.php
@@ -7,18 +7,19 @@ include("includes/conexion.php");
     <meta charset="UTF-8">
     <title>Gestor de Alquiler de Vehículos</title>
     <link rel="stylesheet" href="css/estilo.css">
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 </head>
 <body>
     <h1>Gestor de Alquiler de Vehículos</h1>
 
     <nav>
         <ul>
-            <li><a href="vehiculos/registrar.php">Registrar Vehículo</a></li>
-            <li><a href="vehiculos/listar.php">Listar Vehículos</a></li>
-            <li><a href="clientes/registrar.php">Registrar Cliente</a></li>
-            <li><a href="clientes/listar.php">Listar Clientes</a></li>
-            <li><a href="reservas/registrar.php">Crear Reserva</a></li>
-            <li><a href="reservas/listar.php">Historial de Reservas</a></li>
+            <li><a href="vehiculos/registrar.php"><i class="fas fa-car"></i> Registrar Vehículo</a></li>
+      <li><a href="vehiculos/listar.php"><i class="fas fa-list"></i> Listar Vehículos</a></li>
+      <li><a href="clientes/registrar.php"><i class="fas fa-user-plus"></i> Registrar Cliente</a></li>
+      <li><a href="clientes/listar.php"><i class="fas fa-users"></i> Listar Clientes</a></li>
+      <li><a href="reservas/registrar.php"><i class="fas fa-key"></i> Crear Reserva</a></li>
+      <li><a href="reservas/listar.php"><i class="fas fa-history"></i> Historial de Reservas</a></li>
         </ul>
     </nav>
 </body>
